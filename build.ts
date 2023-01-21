b1339a817f7bb47c2531190b3db9fba73666c396
yukihane <yukihane.feather@gmail.com>
2023-01-21 11:38:58 +0900

    content scripts を準備

--- build.ts
+++ build.ts
@@ -8,6 +8,8 @@ const firefoxFlag = process.argv.includes("--firefox");
 const builder = new Builder({ watchFlag, devFlag, chromeFlag, firefoxFlag });
 builder.addBuildFile("src/popup/index.tsx");
 builder.addStaticFile("src/popup/popup.html");
+builder.addBuildFile("src/content/sc/sc.ts");
+builder.addBuildFile("src/content/oo/oo.tsx");
 builder.addStaticDir("icons");
 
 builder.build();
---
+++ src/content/sc/sc.ts
@@ -0,0 +1,1 @@
+alert("Hello sc");
